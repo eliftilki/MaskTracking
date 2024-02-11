88f3df34f6b3301b5a57bc5d2f96ac938c3d8805
Elif Tilki <134945633+eliftilki@users.noreply.github.com>
2024-02-11 13:25:12 +0300

    Add files via upload

---
+++ Workaround/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maske takip uygulamsına hoşgeldiniz...");
+            Person person1 = new Person();
+            Console.Write("isim:");
+            person1.FirstName =Console.ReadLine();
+            Console.Write("Soyisim:");
+            person1.LastName = Console.ReadLine();
+            Console.Write("Doğum yılı:");
+            person1.DateOfBirthYear = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kimlik no:");
+            person1.NationalIdentity = Convert.ToInt64(Console.ReadLine());
+
+            PttManager pttManager = new PttManager(new CitizenManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+      
+       
+        
+
+    }
+}
